3203b4385043db7c7e6de33955784663794375a9
darkdeaddeset <darkdeaddeset@gmail.com>
2021-09-14 00:53:29 +0300

    One version

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ftp_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write("Example IP host: ftp://xxx.xxx.xx.xx\n");
+            Console.Write("Enter IP host: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Enter login of user: ");
+            string user = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Enter password: ");
+            string pass = Console.ReadLine();
+            Console.WriteLine();
+
+            ftp_client ftp_Client = new ftp_client(ip, user, pass);
+
+            Console.WriteLine("1) Upload");
+            Console.WriteLine("2) Download");
+            Console.WriteLine("3) Rename");
+            Console.WriteLine("4) Delete");
+            Console.WriteLine("5) Create Directory");
+            Console.WriteLine("6) Details list of directory");
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D1 || Console.ReadKey(true).Key == ConsoleKey.NumPad1)
+            {
+                
+                Console.Write("Enter name of file: ");
+                string file = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter local file");
+                string upload_file = Console.ReadLine();
+
+                Thread d1 = new Thread( ()=> ftp_Client.Upload(file, upload_file));
+                d1.Start();
+
+                Console.WriteLine("Operationd upload completed!");
+            }
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D2 || Console.ReadKey(true).Key == ConsoleKey.NumPad2)
+            {
+                Console.Write("Enter name of file: ");
+                string file = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter local file: ");
+                string download_file = Console.ReadLine();
+
+                Thread d1 = new Thread(() => ftp_Client.Download(file, download_file));
+                d1.Start();
+
+                Console.WriteLine("Operationd download completed!");
+            }
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D3 || Console.ReadKey(true).Key == ConsoleKey.NumPad3)
+            {
+                Console.Write("Enter name of file: ");
+                string file = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter local file: ");
+                string rename_file = Console.ReadLine();
+
+                Thread d1 = new Thread(() => ftp_Client.Rename(file, rename_file));
+                d1.Start();
+
+                Console.WriteLine("Operationd rename completed!");
+            }
+
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D4 || Console.ReadKey(true).Key == ConsoleKey.NumPad4)
+            {
+                Console.Write("Enter name of file: ");
+                string file = Console.ReadLine();
+                Console.WriteLine();
+                ftp_Client.Delete(file);
+
+                Console.WriteLine("Operationd delete completed!");
+            }
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D5 || Console.ReadKey(true).Key == ConsoleKey.NumPad5)
+            {
+                Console.Write("Enter name for new directory: ");
+                string new_directory = Console.ReadLine();
+                ftp_Client.createDirectory(new_directory);
+
+                Console.WriteLine("Operationd new directory completed!");
+            }
+
+            if (Console.ReadKey(true).Key == ConsoleKey.D6 || Console.ReadKey(true).Key == ConsoleKey.NumPad6)
+            {
+                Console.Write("Enter name of directory: ");
+                string directory = Console.ReadLine();
+                string[] detailListDirectorty = ftp_Client.directoryListDetails(directory);
+                for (int i = 0; i < detailListDirectorty.Count(); i++)
+                    Console.WriteLine(detailListDirectorty[i]);
+
+                Console.WriteLine("Operationd Details List of Directory completed!");
+            }   
+        }
+    }
+}
---
+++ ftp_client.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Linq;
+
+namespace ftp_client
+{
+    class ftp_client
+    {
+        private string host = null;
+        private string username = null;
+        private string password = null;
+        private FtpWebRequest ftpRequest = null;
+        private FtpWebResponse ftpResponse = null;
+        private Stream ftpStream = null;
+        private int bufferSize = 2048;
+
+        public ftp_client(string hostIP, string UserName, string Password)
+        {
+            host = hostIP;
+            username = UserName;
+            password = Password;
+        }
+
+        public void Download(string remoteFile, string localFile)
+        {
+                ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + remoteFile);
+                ftpRequest.Credentials = new NetworkCredential(username, password);
+                ftpRequest.KeepAlive = true;
+                ftpRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+                ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+                ftpStream = ftpResponse.GetResponseStream();
+                FileStream localFileStream = new FileStream(localFile, FileMode.Open);
+                byte[] byteBuffer = new byte[bufferSize];
+                int byteRead = ftpStream.Read(byteBuffer, 0, bufferSize);
+                
+                while (byteRead > 0)
+                {
+                    localFileStream.Write(byteBuffer, 0, byteRead);
+                    byteRead = ftpStream.Read(byteBuffer, 0, bufferSize);
+                }                               
+                localFileStream.Close();
+                ftpStream.Close();
+                ftpResponse.Close();
+                ftpRequest = null;            
+        }
+
+        public void Upload(string remoteFile, string localFile)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + remoteFile);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            ftpStream = ftpRequest.GetRequestStream();
+            FileStream localFileStream = new FileStream(localFile, FileMode.Open);
+            byte[] byteBuffer = new byte[bufferSize];
+            int byteSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+
+            while (byteSent > 0)
+            {
+                ftpStream.Write(byteBuffer, 0, byteSent);
+                byteSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+            }
+            localFileStream.Close();
+            ftpStream.Close();
+            ftpRequest = null;
+        }
+
+        public void Delete(string deleteFile)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + deleteFile);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.DeleteFile;
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpResponse.Close();
+            ftpRequest = null;
+        }
+
+        public void DeleteDir(string deleteDir)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + deleteDir);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.RemoveDirectory;
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpResponse.Close();
+            ftpRequest = null;
+        }
+
+        public void Rename(string currentFileNameAndPath, string newFileName)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + currentFileNameAndPath);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.Rename;
+            ftpRequest.RenameTo = newFileName;
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpResponse.Close();
+            ftpRequest = null;
+        }
+
+        public void createDirectory(string newDirectory)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + newDirectory);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.MakeDirectory;
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpResponse.Close();
+            ftpRequest = null;
+        }
+
+        public string[] directoryListDetails(string directory)
+        {
+            ftpRequest = (FtpWebRequest)FtpWebRequest.Create(host + "/" + directory);
+            ftpRequest.Credentials = new NetworkCredential(username, password);
+            ftpRequest.KeepAlive = true;
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            ftpResponse = (FtpWebResponse)ftpRequest.GetResponse();
+            ftpStream = ftpResponse.GetResponseStream();
+            StreamReader ftpReader = new StreamReader(ftpStream);
+            string readerDirectory = null;
+            
+            while (ftpReader.Peek() != -1)
+            {
+                readerDirectory += ftpReader.ReadLine() + "|";
+            }
+
+            ftpReader.Close();
+            ftpStream.Close();
+            ftpResponse.Close();
+            ftpRequest = null;
+
+            string[] directoryList = readerDirectory.Split("|".ToCharArray());
+            return directoryList;
+        }
+    }
+}
